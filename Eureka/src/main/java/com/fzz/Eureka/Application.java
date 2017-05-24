4275568c88ee68df1447b7bdfea5cec3bee0e8f8
fangzongzhou <739560832@qq.com>
2017-05-24 08:39:40 +0800

    Initial commit

---
+++ Eureka/src/main/java/com/fzz/Eureka/Application.java
@@ -0,0 +1,12 @@
+package com.fzz.Eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by fzz on 2017/5/24.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+}
---
+++ asdf/src/main/java/com/fzz/Application.java
@@ -0,0 +1,17 @@
+package com.fzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by fzz on 2017/5/24.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
