eca0f06a538e065e9829709ad1f0ad7d2f6da0bf
fangzongzhou <739560832@qq.com>
2017-05-25 08:27:12 +0800

    添加状态监控

---
+++ service-ribbon/src/main/java/com/fzz/controller/HelloControler.java
@@ -0,0 +1,23 @@
+package com.fzz.controller;
+
+import com.fzz.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fzz on 2017/5/24.
+ */
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+
+
+}
